9b122955d1cddd679407b5f9c29157b3908e5843
adarshmelethil <adarshmelethil@gmail.com>
2019-11-07 14:40:21 -0700

    created a python package

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from os import environ
+
+setup(
+  name="Workshops",
+  version="0.0.1",
+  package_dir={"": "src"},
+  packages=find_packages("src"),
+  include_package_data=True,
+  install_requires=[],
+)
